7bcee295e491e8cbc8188abf00a459a0de4d7754
Dane Middleton <middletondane@gmail.com>
2018-05-03 19:26:29 -0500

    add existing array

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,9 +1,11 @@
 'use strict'
+const names = ['bob', 'jan']
 
 const printListOfOlympians = (array) =>
 
-array.forEach(function(item) {
-  console.log(item);
+array.forEach(function(name) {
+  console.log(name);
 });
 
-printListOfOlympians(['mark','george','sarah','katie','mary','judy', 'colleen'])
+// printListOfOlympians(['mark','george','sarah','katie','mary','judy', 'colleen'])
+printListOfOlympians(names);
