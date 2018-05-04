1d22db9d33247effc641c0259de537c95c37c1e7
Dane Middleton <middletondane@gmail.com>
2018-05-03 19:07:24 -0500

    created file and array with names

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const printListOfOlympians = (array) =>
+
+array.forEach((item))
+
+array.forEach(function(item) {
+  console.log(item);
+});
+
+printListOfOlympians(['mark','george','sarah','katie','mary'])
