bc3a54d420bd4170816463ccbcc928751213926c
Dane Middleton <middletondane@gmail.com>
2018-05-07 14:52:26 -0500

    passing tests

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -11,21 +11,25 @@ const rl = readline.createInterface({
 // push function is mutable adds an item (argument) to an array as the last item in the original array returning the new array length
 
 //  movePiece function uses pop function to remove last item and takes the popped item and pushes it
-// why doesnt stacks.startStack work?
+// -------why doesnt stacks.startStack work?----------
 
 //  isLegal function checks if the popped item is less than the current last item in the destination array. access the last item with stacks.(array)[array.length - 1]. if the array is empty the popped item can be moved to the empty array(might already pass). return true if conditions are met or console.log an error message.
 
-// checkForWin function checks if stacks.b === [4, 3, 2, 1] || stacks.c === [4, 3, 2, 1], console.log win
+// checkForWin function checks if stacks.b == [4, 3, 2, 1] || stacks.c == [4, 3, 2, 1], console.log win.
+//-----create variable winning array and compare stacks.b and stacks.c with ==,not working------
+// index[3] == 1 worked to pass test
 
 // towersOfHanoi function takes two arguments(startStack,endStack)
 // start with movePiece or isLegal function. likely need to start with movePiece so there are items to check in isLegal. end with checkForWin function
 
+// ----console.log & return throws error, return in checkforwin function and log in towersofhanoi function------
 
-const stacks = {
+let stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
+
 
 function printStacks() {
   console.log("a: " + stacks.a);
@@ -35,30 +39,36 @@ function printStacks() {
 
 function movePiece(startStack, endStack) {
 
-stacks[endStack].push(stacks[startStack].pop());
+  stacks[endStack].push(stacks[startStack].pop());
 
 }
 
 function isLegal(startStack, endStack) {
-if (stacks[startStack][stacks[startStack].length - 1] > stacks[endStack][stacks[endStack].length - 1]) {console.log("false")}
-else if (stacks[endStack] == 0) {console.log("true")}
-else {console.log("true")}
+  if (stacks[startStack][stacks[startStack].length - 1] > stacks[endStack][stacks[endStack].length - 1]) {
+    return false
+  } else if (stacks[endStack] == 0) {
+    return true;
+  } else {
+    return true;
+  }
 
 }
 
 function checkForWin() {
-// if (stacks = { a: [], b: [4, 3, 2, 1], c: [] } || stacks = { a: [], b: [], c: [4, 3, 2, 1] }) {
-//   return true
-// } else {
-//   return false
-// }
-
-}
+  if (stacks.b[3] == 1 || stacks.c[3] == 1) {
+    return true
+  } else {
+    return false
+  }
+};
 
 function towersOfHanoi(startStack, endStack) {
-if (isLegal(startStack, endStack) == true);
-{movePiece(startStack, endStack)};
-
+  if (isLegal(startStack, endStack)) {
+    movePiece(startStack, endStack);
+    if (checkForWin()) {
+      console.log('you moved a tower')
+    };
+  }
 }
 
 function getPrompt() {
@@ -78,7 +88,11 @@ if (typeof describe === 'function') {
   describe('#towersOfHanoi()', () => {
     it('should be able to move a block', () => {
       towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
+      assert.deepEqual(stacks, {
+        a: [4, 3, 2],
+        b: [1],
+        c: []
+      });
     });
   });
 
@@ -102,9 +116,17 @@ if (typeof describe === 'function') {
   });
   describe('#checkForWin()', () => {
     it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+      stacks = {
+        a: [],
+        b: [4, 3, 2, 1],
+        c: []
+      };
       assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
+      stacks = {
+        a: [1],
+        b: [4, 3, 2],
+        c: []
+      };
       assert.equal(checkForWin(), false);
     });
   });
